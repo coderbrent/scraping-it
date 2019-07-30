f87d6a77ba2899c471d06d00c67a8393f7a6630e
Brent Abruzese <brent.abruzese@gmail.com>
2019-07-30 13:46:05 -0400

    added Article import to index.js

--- app.js
+++ app.js
@@ -45,8 +45,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(port, () => {
-  console.log(`The app is now listening on port ${port}`)
-});
-
 module.exports = app;
